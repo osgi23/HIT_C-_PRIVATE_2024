fd9e80f87d89f26299286eb054c95dacc8614f64
osgi23 <manhquan03122005@gmail.com>
2024-11-08 07:16:08 +0700

    BTVN Buoi2

---
+++ BTVN_Buoi2/bai2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = Console.ReadLine();
+            
+            if(s.Length % 2 != 0)
+                Console.WriteLine("NO");
+            else
+            {
+                int i = 0, j = s.Length - 1;
+                bool check = true;
+                while(i < j)
+                {
+                    if (s[i] != s[j])
+                    {
+                        check = false;
+                        break;
+                    }
+                    ++i;
+                    --j;
+                }
+                if (check)
+                    Console.WriteLine("YES");
+                else
+                    Console.WriteLine("NO");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
